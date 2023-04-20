841fe8d7340ff3803b834b8c424a185daad45116
Lucien <lucien.zak@laplateforme.io>
2023-04-20 17:35:18 +0200

    ✨Ajout un éditeur et un menu de départ

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import os
 from pygame.locals import *
 from modules.player import Player
 from modules.box import Box
@@ -13,18 +14,30 @@ WIDTH = 1024
 HEIGHT = 720
 pygame.display.set_caption('Sokoban')
 MAPS = [
-    ['.', '.', '.','.', 'W', 'W','W', 'W', 'W','.','.','.','.','.','.','.','.','.',],
-    ['.', '.', '.','.', 'W', '.','.', '.', 'W','.','.','.','.','.','.','.','.','.',],
-    ['.', '.', 'W','W', 'W', '.','P', '.', 'W','W','.','.','.','.','.','.','.','.',],
-    ['.', '.', 'W','.', '.', '.','.', 'B', '.','W','.','.','.','.','.','.','.','.',],
-    ['W', 'W', 'W','.', 'W', '.','W', 'W', '.','W','.','.','W','W','W','W','W','W',],
-    ['W', '.', '.','.', 'W', '.','W', 'W', '.','W','W','W','W','.','.','.','H','W',],
-    ['W', '.', '.','.', '.', 'B','.', '.', '.','.','.','.','.','.','.','.','H','W',],
-    ['W', 'W', 'W','W', '.', '.','.', '.', '.','B','.','.','.','.','.','.','H','W',],
-    ['.', '.', '.','W', '.', 'B','W', 'W', 'W','.','.','W','W','.','.','.','H','W',],
-    ['.', '.', '.','W', '.', '.','.', '.', '.','.','.','W','W','W','W','W','W','W',],
-    ['.', '.', '.','W', 'W', 'W','W', 'W', 'W','.','.','W','.','.','.','.','.','.',],
-    ['.', '.', '.','.', '.', '.','.', '.', '.','W','W','.','.','.','.','.','.','.',],
+    ['.', '.', '.', '.', 'W', 'W', 'W', 'W', 'W',
+        '.', '.', '.', '.', '.', '.', '.', '.', '.',],
+    ['.', '.', '.', '.', 'W', '.', '.', '.', 'W',
+        '.', '.', '.', '.', '.', '.', '.', '.', '.',],
+    ['.', '.', 'W', 'W', 'W', '.', 'P', '.', 'W',
+        'W', '.', '.', '.', '.', '.', '.', '.', '.',],
+    ['.', '.', 'W', '.', '.', '.', '.', 'B', '.',
+        'W', '.', '.', '.', '.', '.', '.', '.', '.',],
+    ['W', 'W', 'W', '.', 'W', '.', 'W', 'W', '.',
+        'W', '.', '.', 'W', 'W', 'W', 'W', 'W', 'W',],
+    ['W', '.', '.', '.', 'W', '.', 'W', 'W', '.',
+        'W', 'W', 'W', 'W', '.', '.', '.', 'H', 'W',],
+    ['W', '.', '.', '.', '.', 'B', '.', '.', '.',
+        '.', '.', '.', '.', '.', '.', '.', 'H', 'W',],
+    ['W', 'W', 'W', 'W', '.', '.', '.', '.', '.',
+        'B', '.', '.', '.', '.', '.', '.', 'H', 'W',],
+    ['.', '.', '.', 'W', '.', 'B', 'W', 'W', 'W',
+        '.', '.', 'W', 'W', '.', '.', '.', 'H', 'W',],
+    ['.', '.', '.', 'W', '.', '.', '.', '.', '.',
+        '.', '.', 'W', 'W', 'W', 'W', 'W', 'W', 'W',],
+    ['.', '.', '.', 'W', 'W', 'W', 'W', 'W', 'W',
+        '.', '.', 'W', '.', '.', '.', '.', '.', '.',],
+    ['.', '.', '.', '.', '.', '.', '.', '.', '.',
+        'W', 'W', '.', '.', '.', '.', '.', '.', '.',],
 
 ]
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -39,23 +52,85 @@ def generateMaps(MAPS):
         for char in row:
             nbCol += 1
             if char == 'W':
-                wall = Wall((nbCol - 1) * 50, (nbRow - 1) * 50, 50, 50, 'black', screen)
+                wall = Wall((nbCol - 1) * 50, (nbRow - 1)
+                            * 50, 50, 50, 'black', screen)
                 walls.append(wall)
             if char == 'H':
                 hole = Hole((nbCol - 1) * 50, (nbRow - 1) * 50, 50, screen)
                 holes.append(hole)
             if char == 'B':
-                box = Box((nbCol - 1) * 50, (nbRow - 1) * 50, 50, 50, (0, random.randrange(0, 255), 255), screen)
+                box = Box((nbCol - 1) * 50, (nbRow - 1) * 50, 50, 50,
+                          (0, random.randrange(0, 255), 255), screen)
                 boxes.append(box)
             if char == 'P':
-                player = Player((nbCol - 1) * 50, (nbRow - 1) * 50, 50, 50, (255, 0, 0), screen)
+                player = Player((nbCol - 1) * 50, (nbRow - 1)
+                                * 50, 50, 50, (255, 0, 0), screen)
     return nbRow, nbCol, walls, boxes, holes, player
-
 
 
 running = 1
 
 boxInHole = []
+
+
+def editorLoop():
+
+    def itemSelector(event):
+        items = ['box.png', 'wall.png', 'hole']
+        if event.type == KEYDOWN:
+            if event.key == pygame.K_1:
+                print(items[0])
+                return items[0]
+            if event.key == K_2:
+                return items[1]
+            if event.key == K_3:
+                return items[2]
+
+        return image
+
+    maps = []
+    choosenGrid = pygame.draw.rect(screen, (255, 255, 255), (0, 0, 50, 50), 1)
+    items = []
+    image = 'box.png'
+
+    while running:
+        screen.fill((120, 120, 120))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                if event.key == K_e:
+                    gameLoop()
+            image = itemSelector(event)
+        mousPos = pygame.mouse.get_pos()
+        if image != 'hole':
+            choosenGrid = pygame.draw.rect(
+                screen, ('black'), (mousPos[0] - mousPos[0] % 50, mousPos[1] - mousPos[1] % 50, 50, 50), 1)
+            choosenGridImage = pygame.transform.scale(
+                pygame.image.load(os.path.join("assets", image)), (50, 50))
+            screen.blit(
+                choosenGridImage, (mousPos[0] - mousPos[0] % 50, mousPos[1] - mousPos[1] % 50))
+        else:
+            choosenGrid = pygame.draw.circle(
+                screen, (0, 0, 0), (mousPos[0] - mousPos[0] % 50 + 25, mousPos[1] - mousPos[1] % 50 + 25), 25)
+        if pygame.mouse.get_pressed()[0]:
+            if image != 'hole':
+                items.append([image, mousPos[0] - mousPos[0] %
+                             50, mousPos[1] - mousPos[1] % 50])
+            else:
+                items.append([image, mousPos[0] - mousPos[0] %
+                             50 + 25, mousPos[1] - mousPos[1] % 50 + 25])
+        for item in items:
+            if item[0] != 'hole':
+                itemImage = pygame.transform.scale(pygame.image.load(
+                    os.path.join("assets", item[0])), (50, 50))
+                screen.blit(itemImage, (item[1], item[2]))
+            else:
+                pygame.draw.circle(screen, (0, 0, 0), (item[1], item[2]), 25)
+        pygame.display.flip()
+
 
 def gameLoop():
     global boxInHole
@@ -75,13 +150,13 @@ def gameLoop():
             isInHole, indexBox = hole.boxIsOnHole(boxes)
             if isInHole:
                 print(indexBox)
-                
+
                 boxInHole.append(indexBox)
                 boxes.pop(indexBox - 1)
                 hole.validateHole()
                 # if boxInHole == len(boxes):
                 #     print('You win')
-                #     pygame.quit()    
+                #     pygame.quit()
         for wall in walls:
             wall.draw()
         for box in boxes:
@@ -94,7 +169,25 @@ def gameLoop():
         pygame.display.flip()
 
 
-
 while True:
-    gameLoop()
-
+    fonts = pygame.font.get_fonts()
+    font = pygame.font.SysFont('arial', 30)
+    text = font.render('Appuiyer sur G pour lancer le jeu',
+                       True, (255, 255, 255))
+    text1 = font.render('Appuyer sur E pour lancer l\'editeur', True, (255, 255, 255))
+    text2 = font.render('Appuyer sur Echap pour quitter', True, (255, 255, 255))
+    screen.blit(text, (WIDTH / 2 - 150, HEIGHT / 2 - 50))
+    screen.blit(text1, (WIDTH / 2 - 150, HEIGHT / 2))
+    screen.blit(text2, (WIDTH / 2 - 150, HEIGHT / 2 + 50))
+    # screen.fill((120, 120, 120))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+            if event.key == K_e:
+                editorLoop()
+            if event.key == K_g:
+                gameLoop()
+    pygame.display.flip()
